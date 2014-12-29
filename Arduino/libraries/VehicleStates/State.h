6eba2553017ae2909b015d98a1204fa99883f0be
Mathieu <mbinette@live.ca>
2014-12-28 22:18:45 -0500

    Added a LinearStateMachine to the system, to register linear sequences of states.

--- Arduino/libraries/VehicleStates/State.h
+++ Arduino/libraries/VehicleStates/State.h
@@ -9,6 +9,7 @@ namespace Vehicle
       {
       public:
         State() : _active(false) {};
+        virtual ~State() {};
 
         virtual void Start() 
         { 
@@ -34,6 +35,68 @@ namespace Vehicle
 
       private:
         bool _active;
+      };
+
+      struct LinearStateMachineNode{
+        State* state;
+        LinearStateMachineNode* next;
+      };
+
+      class LinearStateMachine
+      {
+        static const int NULL = 0;
+        int count;
+        LinearStateMachineNode* root;
+
+      public:
+        LinearStateMachine()
+        { 
+          count = 0;
+          root = NULL;
+        }
+
+        ~LinearStateMachine()
+        { 
+          while(root != NULL)
+          {
+            LinearStateMachineNode* old = root;
+            root = root->next;
+            delete old;
+          }
+        }
+
+        void Add(State* state)
+        {
+          LinearStateMachineNode* node = new LinearStateMachineNode;
+          node->state = state;
+          node->next = NULL;
+          if(root == NULL)
+            root = node;
+          else{
+            LinearStateMachineNode* last = root;
+            while(last->next != NULL)
+            {
+              last = last->next;
+            }
+            last->next = node;
+          }
+          count++;
+        }
+
+        void Execute()
+        {
+          LinearStateMachineNode *node = root;
+          while(node)
+          {
+            node->state->Start();
+            node->state->Update();
+            while(node->state->IsActive()){
+              node->state->Execute();
+              node->state->Update();
+            }
+            node = node->next;
+          }
+        }
       };
   }
 }
---
+++ Arduino/tests/StateMachineTests/StateMachineTests.ino
@@ -0,0 +1,46 @@
+#include <State.h>
+using namespace Vehicle::States;
+
+class StateA : public State
+{
+public:
+  StateA() : State() {};
+  ~StateA() { Serial.println("Deleting StateA."); };
+  void Execute() { Serial.println("Executing StateA."); End(); }
+};
+
+class StateB : public State
+{
+public:
+  int timesLeft;
+  StateB(int count) : State() { timesLeft = count;};
+  ~StateB() { Serial.println("Deleting StateB."); };
+  void Execute() { 
+    Serial.println("Executing StateB."); 
+    timesLeft--; 
+    if(timesLeft <= 0)
+      End(); 
+  }
+};
+
+void setup() 
+{
+  Serial.begin(9600);
+  
+  LinearStateMachine machine;
+  machine.Add(new StateA());
+  machine.Add(new StateB(3));
+  
+  Serial.println("Starting the StateMachine.");
+  
+  machine.Execute();
+  
+  Serial.println("Done with the StateMachine.");
+}
+ 
+void loop() 
+{
+  
+} 
+
+
