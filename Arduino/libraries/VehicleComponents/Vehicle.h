e6942c704a281a306a78f15886f4f112d8d6a73d
Mathieu Binette <mbinette91@gmail.com>
2014-12-30 06:14:18 -0500

    COrrected values for shooting on real vehicle.

--- Arduino/libraries/VehicleComponents/Vehicle.h
+++ Arduino/libraries/VehicleComponents/Vehicle.h
@@ -8,9 +8,9 @@
 #include <State.h>
 
 // Pins
-#define VEHICLE_SERVO_PIN           5 //Will be 6
+#define VEHICLE_SERVO_PIN           6
 #define VEHICLE_SENSOR_PIN          A9
-#define STORE_SERVO_PIN             6 //Will be 5
+#define STORE_SERVO_PIN             5
 #define STORE_DETECT_BAG_SWITCH_PIN 22
 //#define SHOOTER_SWITCH_PIN        38
 #define SHOOTER_SENSOR_PIN          A8 
@@ -36,7 +36,7 @@
 #define VEHICLE_SENSOR_MIN_VALUE    0.15
 
 // Store
-#define STORE_SERVO_INITIAL_MICROS  620   // Must be close enough to 600 (STORE_SERVO_MIN_MICROS)
+#define STORE_SERVO_INITIAL_MICROS  613   // Must be close enough to 600 (STORE_SERVO_MIN_MICROS) - Is sometimes 613 or 620
 #define STORE_INITIALIZE_DELAY      3500  // Time to reinitialize the store to STORE_SERVO_INITIAL_MICROS
 #define STORE_SERVO_MICROS_STEP     27    // For 1/8 rotation
 #define STORE_MAX_NUMBER_OF_BAGS    8
@@ -53,7 +53,7 @@
 // Shooter constants
 #define SHOOTER_SENSOR_DEBOUNCE_TIME    100
 #define SHOOTER_MOTOR_REQUIRED_TIME     700   // Time required to power the motor
-#define SHOOTER_LOAD_AND_SHOOT_DELAY    400   // Time it takes to charge and shoot the bag and time until it reaches the hole (millis)
+#define SHOOTER_LOAD_AND_SHOOT_DELAY    100   // Time it takes to charge and shoot the bag and time until it reaches the hole (millis)
                                               // If it cuts too late, no big deal, the elastic will bring it back to initial position
 #define SHOOTER_BAG_FREE_FALL_DELAY     500   // Time for the bag to fall into place. Put some padding in there.
 #define SHOOTER_MOTOR_RDY_TO_RECV_DELAY 1000  // Delay to be sure the system is ready to receive the new bag (for the elastic to bring the launcher back to initial position). Is used after the motor has been shut down and unloading the new one.
